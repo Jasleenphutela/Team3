3dae9a2ced6752c031448f97339c933f50cfa037
Jasleenphutela <100710384+Jasleenphutela@users.noreply.github.com>
2024-08-25 21:40:11 +0530

    Delete eCommerceApp-main directory

--- eCommerceApp-main/ecommerceapp/src/main/java/in/co/hsbc/ecommerceApp/entity/AccessSubscription.java
+++
@@ -1,7 +0,0 @@
-package in.co.hsbc.ecommerceApp.entity;
-
-public class AccessSubscription extends Subscriptions{
-    public AccessSubscription(int subscriptionValidity) {
-        super(subscriptionValidity, "Access","active");
-    }
-}
--- eCommerceApp-main/in/co/hsbc/ecommerceApp/entity/RecurringSubscription.java
+++
@@ -1,8 +0,0 @@
-package in.co.hsbc.ecommerceApp.entity;
-
-public class RecurringSubscription extends Subscriptions{
-
-    public RecurringSubscription(int subscriptionValidity) {
-        super(subscriptionValidity, "Recurring","active");
-    }
-}
